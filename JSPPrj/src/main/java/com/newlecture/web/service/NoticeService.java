982042b3829ece60b4a8347b9b9330a99d16fa66
Jun_Hyeok <ju36380@naver.com>
2022-12-27 10:19:22 +0900

    [update] 서블릿/JSP 강의 72 완료

--- JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
+++ JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -17,15 +17,29 @@ public class NoticeService {
 
 	public List<Notice> getNoticeList(String field, String query, int page){
 		
+		String sql = "SELECT * FROM "
+				+ "(SELECT ROWNUM NUM, N.* "
+				+ "FROM "
+				+ "(SELECT * FROM NOTICE ORDER BY REGDATE DESC) N"
+				+ ") "
+				+ "WHERE NUM BETWEEN 6 AND 10";
+		
 		return null;
 	}
 	
 	public int getNoticeCount() {
 		
-		return getNoticeCount("", "");
+		return getNoticeCount("title", "");
 	}
 	
 	public int getNoticeCount(String field, String query) {
+		
+		String sql = "SELECT * FROM "
+				+ "(SELECT ROWNUM NUM, N.* "
+				+ "FROM "
+				+ "(SELECT * FROM NOTICE ORDER BY REGDATE DESC) N"
+				+ ") "
+				+ "WHERE NUM BETWEEN 6 AND 10";
 		
 		return 0; 
 	}
