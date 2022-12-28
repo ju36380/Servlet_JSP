3755c8e082e2de58bb9bd291309800f083f6272a
Jun_Hyeok <ju36380@naver.com>
2022-12-28 14:11:29 +0900

    [update] 서블릿/JSP 강의 96 완료

--- JSPPrj/src/main/java/com/newlecture/web/controller/admin/notice/RegController.java
+++ JSPPrj/src/main/java/com/newlecture/web/controller/admin/notice/RegController.java
@@ -23,6 +23,10 @@ public class RegController extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String title = request.getParameter("title");
+		
+		System.out.println("title");
+		System.out.println(title);
+		
 		String content = request.getParameter("content");
 		String isOpen = request.getParameter("open");
 		boolean pub = false;
